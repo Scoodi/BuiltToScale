41d3bb63598edf24056343c9ed9fd2088411e63e
Scoodi <60648256+Scoodi@users.noreply.github.com>
2024-08-16 20:22:32 +0100

    Update CharacterScript.cs
    Removed useless bits from char script

--- Assets/Scripts/CharacterScript.cs
+++ Assets/Scripts/CharacterScript.cs
@@ -8,27 +8,12 @@ using Random = UnityEngine.Random;
 
 public class CharacterScript : MonoBehaviour
 {
-    public enum PlayerMode {
-        None,
-        Repair,
-        Dig,
-        Zap
-
-    }
     public static CharacterScript Instance { get; private set; }
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private GroundDetector feet;
     [SerializeField] private Animator playerAnim;
-    [SerializeField] private RadialMenu radialMenu;
     [SerializeField] private AudioSource playerAudio;
-    [SerializeField] private Transform zapCastPoint;
     [SerializeField] private SpriteRenderer playerSpriteRend;
-
-    [Header("HUD")]
-
-    [SerializeField] private TMP_Text modeDisplay;
-    [SerializeField] private HealthDisplay healthDisplay;
-    
 
     [SerializeField] private AudioSource playerOneShotAudio;
 
@@ -50,39 +35,10 @@ public class CharacterScript : MonoBehaviour
     [SerializeField] private float timeBeforeDownforce = 1f;
     [SerializeField] private float coyoteTime = 0.1f;
     [SerializeField] private float jumpBuffer = 0.15f;
-    //Private Gameplay Vars
-    private int health = 3;
-    private bool alive = true;
-    private bool invincible = false;
-    private ICanBeRepaired repairInteraction;
-    private ICanBeDug digInteraction;
-
-    private bool menuActive = false;
-    
-    [Header("Gameplay Vars")]
-    public bool[] modesUnlocked;
-    [SerializeField] private bool[] modesUnlockedAtStart;
-    [SerializeField] private PlayerMode currentMode = PlayerMode.None;
-    [SerializeField] private Color invincibilityColor;
-    [SerializeField] private TMP_Text newModeDisplay;
-    [SerializeField] private GameObject deathMenu;
-    [SerializeField] private Sprite deadSprite;
-    [SerializeField] private float invincibilityTime = 2f;
-    
 
     [Header("Sound Effects")]
     [SerializeField] private AudioClip[] jumpSfx;
     [SerializeField] private AudioClip landSfx;
-    [SerializeField] private AudioClip[] sillySfx;
-    [SerializeField] private AudioClip[] zapSfx;
-    [SerializeField] private AudioClip digSfx;
-    [SerializeField] private AudioClip[] fixSfx;
-    [SerializeField] private AudioClip[] modeUnlockSfx;
- 
-    [Header("Visual Effects")]
-    [SerializeField] private GameObject zapEffect;
-    [SerializeField] private ParticleSystem digParticles;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -94,69 +50,40 @@ public class CharacterScript : MonoBehaviour
         {
             Instance = this;
         }
-        transform.position = LevelScript.Instance.entryPoints[DontDestroyOnLoadData.Instance.entryPoint].position;
         LoadProgress();
-        for (int i = 0; i < modesUnlocked.Length; i++)
-        {
-            modesUnlockedAtStart[i] = modesUnlocked[i];
-        }
-        radialMenu.BuildMenu(modesUnlocked);
         UpdateUI();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (alive && !PauseScript.Instance.paused)
+        ProcessInput();
+        if (horizontalMove != 0)
         {
-            ProcessInput();
-            if (horizontalMove != 0)
-            {
-                Move();
-            }
-            else if (playerAudio.isPlaying)
-            {
-                playerAudio.Pause();
-            }
-            if (!onGround)
-            {
-                timeInAir += Time.deltaTime;
-            }
-            UpdateAnimatorVars();
+            Move();
         }
+        else if (playerAudio.isPlaying)
+        {
+            playerAudio.Pause();
+        }
+        if (!onGround)
+        {
+            timeInAir += Time.deltaTime;
+        }
+        UpdateAnimatorVars();
     }
     public void LoadProgress()
     {
-        currentMode = DontDestroyOnLoadData.Instance.currentPlayerMode;
-        modesUnlocked = DontDestroyOnLoadData.Instance.modesUnlocked;
 
     }
 
     public void SaveProgress()
     {
-        DontDestroyOnLoadData.Instance.currentPlayerMode = currentMode;
-        DontDestroyOnLoadData.Instance.modesUnlocked = modesUnlocked;
+
     }
 
     void UpdateUI() {
-        string modeName = "Uninitialised";
-        switch (currentMode)
-        {
-            case PlayerMode.None:
-                modeName = "legMODE";
-                break;
-            case PlayerMode.Repair:
-                modeName = "fixMODE";
-                break;
-            case PlayerMode.Dig:
-                modeName = "digMODE";
-                break;
-            case PlayerMode.Zap:
-                modeName = "zapMODE";
-                break;
-        }
-        modeDisplay.text = modeName;
-        healthDisplay.UpdateDisplay(health);
+
     }
 
     void ProcessInput () {
@@ -176,66 +103,17 @@ public class CharacterScript : MonoBehaviour
             endingJump = true;
             ApplyDownForce();
         }
-        if (Input.GetKeyDown(KeyCode.Q)) {
-            ToggleRadialMenu();
-        }
-        if (Input.GetKeyDown(KeyCode.E)) {
-            DoModeAction();
-        }
-        if (Input.GetKeyDown(KeyCode.I))
-        {
-            UnlockMode((PlayerMode)Random.Range(0, 4));
-        }
     }
-    void DoModeAction () {
-        switch (currentMode)
-        {
-            case PlayerMode.None:
-                playerOneShotAudio.PlayOneShot(sillySfx[Random.Range(0, sillySfx.Length)]);
-                break;
-            case PlayerMode.Repair:
-                
-                if (repairInteraction != null && !repairInteraction.repaired)
-                {
-                    repairInteraction.Repair();
-                    playerOneShotAudio.PlayOneShot(fixSfx[Random.Range(0, fixSfx.Length)]);
-                }
-                break;
-            case PlayerMode.Dig:
-                playerOneShotAudio.PlayOneShot(digSfx);
-                if (digInteraction != null)
-                {
-                    digInteraction.Dig();
-                    StartCoroutine(FreezePlayer(1f));
-                    digParticles.Play();
-                }
-                break;
-            case PlayerMode.Zap:
-                Zap();
-                break;
-        }
-    }
+
     void OnTriggerEnter2D (Collider2D col) {
-        if (col.gameObject.TryGetComponent(out ICanBeRepaired repair)) {
-            repairInteraction = repair;
-        } else if (col.gameObject.TryGetComponent(out ICanBeDug dig)) {
-            digInteraction = dig;
-        }
+
     }
     void OnTriggerExit2D (Collider2D col) {
-        if (col.gameObject.TryGetComponent(out ICanBeRepaired repair)) {
-            if (repair == repairInteraction) {
-                repairInteraction = null;
-            }
-        } else if (col.gameObject.TryGetComponent(out ICanBeDug dig)) {
-            if (dig == digInteraction) {
-                digInteraction = null;
-            }
-        }
+
     }
     void UpdateAnimatorVars () {
         playerAnim.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
-        playerAnim.SetInteger("Mode", (int)currentMode);
+
     }
     void Jump () {
         rb.velocity = new Vector2(rb.velocity.x,jumpForce);
@@ -281,113 +159,16 @@ public class CharacterScript : MonoBehaviour
         }
     }
 
-    void Zap () {
-        Instantiate(zapEffect, zapCastPoint.position,zapCastPoint.localRotation,zapCastPoint);
-        playerOneShotAudio.PlayOneShot(zapSfx[Random.Range(0,zapSfx.Length)]);
-        Collider2D[] cols = Physics2D.OverlapBoxAll(zapCastPoint.position,Vector2.one,0);
-        foreach (Collider2D col in cols) {
-            if (col.gameObject.TryGetComponent(out ICanBeZapped zappable)) {
-                zappable.GetZapped();
-            }
-        }
-    }
     void Flip() {
         facingRight = !facingRight;
         Vector3 theScale = transform.localScale;
 		theScale.x *= -1;
 		transform.localScale = theScale;
     }
-    public void UnlockMode (PlayerMode mode)
-    {
-        modesUnlocked[(int)mode] = true;
-        newModeDisplay.gameObject.SetActive(true);
-        string modeName = "Uninitialised";
-        switch (mode)
-        {
-            case PlayerMode.None:
-                modeName = "legMODE";
-                break;
-            case PlayerMode.Repair:
-                modeName = "fixMODE";
-                break;
-            case PlayerMode.Dig:
-                modeName = "digMODE";
-                break;
-            case PlayerMode.Zap:
-                modeName = "zapMODE";
-                break;
-        }
-        newModeDisplay.text = modeName + " UNLOCKED!";
-        StartCoroutine(SetInactiveAfterTime(4f,newModeDisplay.gameObject));
-        AudioSource.PlayClipAtPoint(modeUnlockSfx[(int)mode - 1], transform.position, 0.5f);
-        radialMenu.BuildMenu(modesUnlocked);
-    }
 
     IEnumerator SetInactiveAfterTime (float time, GameObject obj)
     {
         yield return new WaitForSeconds (time);
         obj.SetActive (false);
-    }
-    public void SwapMode (PlayerMode mode) {
-        currentMode = mode;
-        UpdateUI();
-        Debug.Log("mode = " + mode);
-    }
-    public void ToggleRadialMenu () {
-        menuActive = !menuActive;
-        radialMenu.gameObject.SetActive(menuActive);
-    }
-
-    public void TakeDamage(int amount) {
-        if (!invincible && alive)
-        {
-            health -= amount;
-            UpdateUI();
-            if (health <= 0)
-            {
-                Die();
-            } else
-            {
-                StartCoroutine(StartInvincibility());
-            }
-        }
-    }
-
-    private IEnumerator FreezePlayer(float timeToFreezeFor)
-    {
-        rb.simulated = alive = false;
-        yield return new WaitForSeconds(timeToFreezeFor);
-        rb.simulated = alive = true;
-    }
-
-
-    public void Die() {
-        for (int i = 0; i < modesUnlockedAtStart.Length; i++)
-        {
-            DontDestroyOnLoadData.Instance.modesUnlocked[i] = modesUnlockedAtStart[i];
-        }
-        playerOneShotAudio.PlayOneShot(sillySfx[6]);
-        DontDestroyOnLoadData.Instance.modesUnlocked = modesUnlockedAtStart;
-        deathMenu.SetActive(true);
-        Destroy(playerAnim);
-        playerSpriteRend.sprite = deadSprite;
-        alive = false;
-    }
-
-    IEnumerator StartInvincibility ()
-    {
-        invincible = true;
-        playerSpriteRend.color = invincibilityColor;
-        yield return new WaitForSeconds(invincibilityTime / 5);
-        playerSpriteRend.color = Color.white;
-        yield return new WaitForSeconds(invincibilityTime / 5);
-        playerSpriteRend.color = invincibilityColor;
-        yield return new WaitForSeconds(invincibilityTime / 5);
-        playerSpriteRend.color = Color.white;
-        yield return new WaitForSeconds(invincibilityTime / 5);
-        playerSpriteRend.color = invincibilityColor;
-        yield return new WaitForSeconds(invincibilityTime / 5);
-        playerSpriteRend.color = Color.white;
-        invincible = false;
     }
 }
