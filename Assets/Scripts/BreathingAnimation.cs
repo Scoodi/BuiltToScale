ce1be98e42cce31fc1c1050a86872eff6419f652
Scoodi <60648256+Scoodi@users.noreply.github.com>
2024-08-20 18:16:07 +0100

    Update BreathingAnimation.cs

--- Assets/Scripts/BreathingAnimation.cs
+++ Assets/Scripts/BreathingAnimation.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,6 +10,7 @@ public class BreathingAnimation : MonoBehaviour
     public float randomVariation = 0.2f;
     public Sprite[] animationSprites;
     public Image display;
+    public SpriteRenderer spriteRenderer;
     private int currentFrame = 0;
     void Start()
     {
@@ -17,14 +19,29 @@ public class BreathingAnimation : MonoBehaviour
 
     IEnumerator BreatheLoop(float time)
     {
-        display.sprite = animationSprites[currentFrame];
-        if (currentFrame < animationSprites.Length - 1 )
+        if (display != null)
         {
-            currentFrame++;
-        } else
-        {
-            currentFrame = 0;
+            display.sprite = animationSprites[currentFrame];
+            if (currentFrame < animationSprites.Length - 1)
+            {
+                currentFrame++;
+            }
+            else
+            {
+                currentFrame = 0;
+            }
+        } else if (spriteRenderer!= null){
+            spriteRenderer.sprite = animationSprites[currentFrame];
+            if (currentFrame < animationSprites.Length - 1)
+            {
+                currentFrame++;
+            }
+            else
+            {
+                currentFrame = 0;
+            }
         }
+        
 
         yield return new WaitForSeconds(time);
         StartCoroutine(BreatheLoop(AnimationRate + Random.Range(-randomVariation,randomVariation)));
